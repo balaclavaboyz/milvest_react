d4aa2b9b0b67c8c01cc8a1bb0f51ab5f33e21485
peter <mvonline@mvonline.com.br>
2022-04-09 22:57:49 -0300

    asd

--- src/Components/Galeria1.jsx
+++ src/Components/Galeria1.jsx
@@ -20,11 +20,6 @@ const galeria1 = importAll(
 const Galeria = () => {
   return (
     <>
-    {/*  */}
-    <IKContext urlEndpoint={urlendpoint}>
-    <IKImage path="galeria_1_milvest_react/IMG_5515_w6EF3SH6es.jpg" width="400"></IKImage>
-    </IKContext>
-    {/*  */}
       <Carousel>
         {
         Object.entries(galeria1).map((t, k) =>
